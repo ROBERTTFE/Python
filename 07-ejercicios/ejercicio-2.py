16d3ab34b0624e1d357a6e9bdac1977a0954a97e
jrv23 <jrv_78@cantv.net>
2026-02-18 13:45:24 +0000

    act

--- 07-ejercicios/ejercicio-2.py
+++ 07-ejercicios/ejercicio-2.py
@@ -2,3 +2,10 @@
 Ejercicio 2.
     - Escribir un script que muestre por pantalla todos los números pares del 1 al 20
 """
+
+# print("\n**************** Ejercicio 2 *****************")
+
+for numero in range(1, 21):
+    if numero % 2 == 0:
+        print(f"El numero {numero} es par")
+        
