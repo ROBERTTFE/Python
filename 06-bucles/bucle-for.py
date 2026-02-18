50f4ecf11cd2d440215f96c1eb727133c7493512
jrv23 <jrv_78@cantv.net>
2026-02-18 12:46:08 +0000

    act

---
+++ 06-bucles/bucle-for.py
@@ -0,0 +1,10 @@
+# Bucle for
+
+contador = 0
+resultado = 0
+
+for contador in range(0,10):
+    print("voy por el " + str(contador))
+    
+    resultado = resultado + contador
+    print(f"El resultado es: {resultado}")
