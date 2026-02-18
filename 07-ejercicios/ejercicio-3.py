f4f4b4cab5e0d854b7bd7b8459a271994996148f
jrv23 <jrv_78@cantv.net>
2026-02-18 13:58:03 +0000

    act

--- 07-ejercicios/ejercicio-3.py
+++ 07-ejercicios/ejercicio-3.py
@@ -1,5 +1,17 @@
 """ 
 Ejercicio 3.
     - Escribir un programa que muestre los cuadrados (un número multiplicado por si mismo) 
-    de los 60 primeros números naturales. Resolverl con for y con while
+    de los 60 primeros números naturales. Resolver con for y con while
 """
+
+# print("\n**************** Ejercicio 3 *****************")
+
+# Con for
+for numero in range(1, 61):
+    print(f"El cuadrado de {numero} es: {numero * numero}")
+    
+# Con while
+contador = 1
+while contador <= 60:
+    print(f"El cuadrado de {contador} es: {contador * contador}")
+    contador = contador + 1
