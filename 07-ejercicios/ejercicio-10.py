e7afd1913069d4410e8166713a62fad044d54dba
jrv23 <jrv_78@cantv.net>
2026-02-19 13:18:55 +0000

    act

--- 07-ejercicios/ejercicio-10.py
+++ 07-ejercicios/ejercicio-10.py
@@ -5,17 +5,33 @@ cuantos han aprobado y cuantos han suspendido.
 
 # print("\n**************** Ejercicio 10 *****************")
 
+# aprobados = 0
+# suspendidos = 0
+# for i in range(8):
+#     nota = float(input(f"Ingrese la nota del alumno {i + 1}: "))
+#     if nota >= 5:
+#         aprobados += 1
+#     else:
+#         suspendidos += 1
+# print(f"Cantidad de alumnos aprobados: {aprobados}")
+# print(f"Cantidad de alumnos suspendidos: {suspendidos}")
+
+# print("\n**************** Ejercicio 10 *****************")
+
+"""
+10. Escribe un programa que pida la nota de 8 alumnos y sacar por pantalla 
+cuantos han aprobado y cuantos han suspendido.
+"""
+
 aprobados = 0
 suspendidos = 0
-for i in range(8):
-    nota = float(input(f"Ingrese la nota del alumno {i + 1}: "))
+na = 1
+while na <= 8:
+    nota = int(input("Introduce la nota: (0-10) "))
     if nota >= 5:
         aprobados += 1
     else:
         suspendidos += 1
-print(f"Cantidad de alumnos aprobados: {aprobados}")
-print(f"Cantidad de alumnos suspendidos: {suspendidos}")
+    na += 1
 
-# print("\n**************** Ejercicio 10 *****************")
-
-
+print(f"Hay {suspendidos} alumnos suspendidos y {aprobados} aprobados")
